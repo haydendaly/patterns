e088e0399fcc6b30cf8f3d2f797c74bd1be1904a
haydendaly <hcd36@cornell.edu>
2022-01-25 11:03:26 -0800

    adds src code

---
+++ code/advanced-patterns/disjoint-sets-union-find/exercises/find-the-duplicate-number.py
@@ -0,0 +1,12 @@
+def find_duplicate(nums):
+    """Takes in an array nums, returns duplicate number"""
+    pass
+
+if __name__ == "__main__":
+    nums = [1, 3, 4, 2, 2]
+    result = 2
+    assert find_duplicate(nums) == result
+
+    nums = [3, 1, 3, 4, 2]
+    result = 3
+    assert find_duplicate(nums) == result
